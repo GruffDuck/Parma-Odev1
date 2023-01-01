b6d61987482784b98489a6f42bed4cf0f4d0ee04
Mehmet Yalınbaş <37973029+GruffDuck@users.noreply.github.com>
2023-01-01 18:52:58 +0300

    Update CubeRotator.cs

--- CubeRotator.cs
+++ CubeRotator.cs
@@ -8,6 +8,6 @@ public class CubeRotator : Singleton<Rotate>
 {
     private void Update()
     {
-     transform.Rotate(new Vector3(90f * Time.deltaTime,0), Space.World);
+     transform.Rotate(new Vector3(75f * Time.deltaTime,0), Space.World);
     }
 }

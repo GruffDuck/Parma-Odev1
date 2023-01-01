7e57b0b529c47ccac387c8a52789e4ea418f16d6
Mehmet Yalınbaş <37973029+GruffDuck@users.noreply.github.com>
2023-01-01 18:52:40 +0300

    Create CubeRotator.cs

---
+++ CubeRotator.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pattern;
+using UnityEngine;
+
+public class CubeRotator : Singleton<Rotate>
+{
+    private void Update()
+    {
+     transform.Rotate(new Vector3(90f * Time.deltaTime,0), Space.World);
+    }
+}
